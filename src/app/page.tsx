c7c2c56e7b5009aee5fbc37c5b4d4173ce21111f
randyazharalman <azharalman17@gmail.com>
2025-11-12 18:02:51 +0700

    05: ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useTRPC } from '@/trpc/client'
 import { useMutation } from '@tanstack/react-query'
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'sonner'
 
 const Page = () => {
+  const [value, setValue] = useState("")
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess : () => {
+      toast.success("Backgroun Job started!")
+    }
+  }))
   return (
     <div className='p-4 max-w-7xl  mx-auto'>
-      <Button onClick={() => invoke.mutate({ text: "tstddd"})}>
+      <Input placeholder='input' value={value} onChange={(e)=> setValue(e.target.value)}/>
+      <Button onClick={() => invoke.mutate({ value: "tstddd"})}>
         Invoke Background Job
       </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
+import { createAgent, openai } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+       const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert Next.js developer. You write readable, maintainable code, you write simple Next.js & React snippets",
+      model: openai({ model: "gpt-4o"}),
+    });
+     const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+
+     return { output}
   },
 );
